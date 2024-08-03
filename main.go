f64f56eddeb135ddf7800b3a75c50ed8026e281a
Nithish S <nithish952001@gmail.com>
2024-08-02 23:12:41 -0700

    add channels

--- main.go
+++ main.go
@@ -1,37 +1,64 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/olahol/melody"
 )
 
-func parseFile(w http.ResponseWriter, r *http.Request) {
+var (
+	channels = make(map[string]*melody.Melody)
+	mu       sync.Mutex
+)
+
+func GetIndexPage(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
 
-func parseChatFile(w http.ResponseWriter, r *http.Request) {
+func GetChannelsPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "channels.html")
+}
+
+func GetChatPage(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "chat.html")
 }
 
 func main() {
 	r := chi.NewRouter()
-	m := melody.New()
 
 	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
 
-	r.Get("/", parseFile)
-	r.Get("/chat", parseChatFile)
+	// Routes
+	r.Get("/", GetIndexPage)
+	r.Get("/channels", GetChannelsPage)
+	r.Get("/chat", GetChatPage)
 
-	r.Get("/ws", func(w http.ResponseWriter, r *http.Request) {
+	// WebSocket route
+	r.Get("/ws/{channel}", func(w http.ResponseWriter, r *http.Request) {
+		channel := chi.URLParam(r, "channel")
+
+		mu.Lock()
+		m, exists := channels[channel]
+		if !exists {
+			m = melody.New()
+			channels[channel] = m
+
+			// Handle incoming WebSocket messages for this channel
+			m.HandleMessage(func(s *melody.Session, msg []byte) {
+				m.Broadcast(msg)
+			})
+		}
+		mu.Unlock()
+
 		m.HandleRequest(w, r)
 	})
 
-	m.HandleMessage(func(s *melody.Session, msg []byte) {
-		m.Broadcast(msg)
-	})
-
+	fmt.Println("Starting server on :3000")
 	http.ListenAndServe(":3000", r)
 }
